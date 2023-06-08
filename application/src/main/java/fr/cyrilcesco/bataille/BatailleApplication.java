e5fe7cd20a497b881a8fc9445d815302f284a55d
Cyril Cesco <cyril.cesco@gmail.com>
2023-06-08 17:37:04 +0200

    initialilsation du projet

---
+++ application/src/main/java/fr/cyrilcesco/bataille/BatailleApplication.java
@@ -0,0 +1,13 @@
+package fr.cyrilcesco.bataille;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatailleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatailleApplication.class, args);
+	}
+
+}
---
+++ application/src/test/java/fr/cyrilcesco/bataille/BatailleApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cyrilcesco.bataille;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatailleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
